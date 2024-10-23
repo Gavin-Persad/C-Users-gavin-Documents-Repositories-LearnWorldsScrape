2ba6ec7891a0e6953116cf0ac46cc0062e1c86dc
Gavin-Persad <gavinapersad@gmail.com>
2024-10-23 10:10:09 +0100

    soup it beautiful

--- learnWorlds.py
+++ learnWorlds.py
@@ -1,1 +1,14 @@
-print("Hello, World!")
+import requests
+from bs4 import BeautifulSoup
+
+def scrape_and_save(url, output_file):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    with open(output_file, 'w', encoding='utf-8') as md_file:
+        md_file.write(soup.prettify())
+
+scrape_and_save(
+    "https://learn.schoolofcode.co.uk/author/pageseditor?page=ebook667931af9ce0f110a50f7467-667931b027143&type=ebook&from=course&course-title-id=bc17-on&unit-id=667931af9ce0f110a50f7467&direct=1", 
+    'output.md'
+)
